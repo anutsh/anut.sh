ce8dd0bc6588bd64b9127a0e6ecb2e86937d5e7a
Daeyun Shin <daeyunshin@gmail.com>
2013-08-03 14:31:45 -0700

    parse json over POST

--- controllers.js
+++ controllers.js
@@ -9,11 +9,13 @@ exports.index = function (req, res) {
 };
 
 exports.submit = function (req, res) {
-    var url = "http://en.wikipedia.org/wiki/Nodejs";
-    var message = "success";
+    var jsonObj = JSON.parse(req.param("jsonData"));
+    var url = jsonObj.url;
 
     var article = extractor.extract(url);
     var terms = extractor.tfidf(article);
+
+    var message = "success";
 
     console.log('Terms = ' + terms);
 
