d2e25cdf1d3ed9b6bf1207b9b8a091f549b12ee9
zdwolfe <wolfe.zach@gmail.com>
2013-08-03 14:03:24 -0700

    Input text javascript events and fix template blowout

--- public/js/app.js
+++ public/js/app.js
@@ -2,20 +2,8 @@ $(function() {
     var $convertButton = $('#convert');
     var $inputUrl = $('#inputUrl');
 
-    $inputUrl.change(function() {
-        console.log('inputUrl.text = ' + $inputUrl.value());
-    });
-
-    // Event handlers for DOM elements
-    $convertButton.click(function() {
-        $.ajax({
-            url: '/',
-            type: 'post',
-            responseType: 'json',
-            data: {
-                url: ''
-            }
-        });
+    $inputUrl.keypress(function() {
+        console.log('inputUrl.text = ' + $inputUrl.val());
     });
 
 });
