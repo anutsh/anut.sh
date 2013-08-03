81572d2d26d068a879c44328e06d00925c28be1f
Josh Davis <josh@joshldavis.com>
2013-08-02 19:31:04 -0700

    Remove run.sh, Gruntfile does it

--- scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-hash nodemon 2> /dev/null || {
-    echo "Oh dear. I require Nodemon, but it's not installed."
-    echo "Install it with: npm install -g nodemon"
-    exit
-}
-
-nodemon -w ./ server.js
