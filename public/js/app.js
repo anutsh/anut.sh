aba11ca498a49860e7fc156cfbd104fb92297404
zdwolfe <wolfe.zach@gmail.com>
2013-08-03 14:07:48 -0700

    Add on paste event

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,16 @@ $(function() {
     var $convertButton = $('#convert');
     var $inputUrl = $('#inputUrl');
 
-    $inputUrl.keypress(function() {
+    function handleUrlChange() {
         console.log('inputUrl.text = ' + $inputUrl.val());
+    }
+
+    $inputUrl.on('paste', function() {
+        // need a small timeout to let input 
+        // element catch up
+        setTimeout(handleUrlChange, 100);
     });
+
+    $inputUrl.on('keypress', handleUrlChange);
 
 });
