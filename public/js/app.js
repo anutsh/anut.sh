227fb4fb4083e003d8b042e38315bbdbcc2fc5f9
zcxcasd <13234656798999@hanmail.net>
2013-08-04 06:14:09 +0000

    aaah

--- public/js/app.js
+++ public/js/app.js
@@ -129,7 +129,7 @@ $(function () {
         });
     }, isUrl = function (url) {
 		var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
-    	return regexp.test(s);
+    	return regexp.test(url);
 	};
 
     //
