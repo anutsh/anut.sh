3878da4982cddaff832d2e8f89ae042143d8da11
zcxcasd <13234656798999@hanmail.net>
2013-08-04 05:19:18 +0000

    added random quotes

--- public/js/text.js
+++ public/js/text.js
@@ -3,12 +3,27 @@
         first: [
             "how does this look?",
             "we know just what you want",
+			"like this nut?",
+			"that looks delicious",
         ],
         multiple: [
             "can i ever satisfy you?",
             "let's be serious now, nuts are nuts.",
             "why can't you just be happy with what you have?",
             "my ex-squirrel was just like you",
+			"here are some different nuts for you",
+			"what do you think of these?",
+			"last nut was just as delicious",
+			"why, you didn't like the last nut?",
+			"nom nom nom I like this nut",
+			"my ex-squirrel used to like that one",
+			"this one looks expecially delicious",
+			"my friend chipmunk recommemded this one",
+			"I'm running out of nuts to give to you",
+			"you're nuts",
+			"you're seriously asking for more nuts?",
+			"did you know that squirrels are born naked, toothless, and blind?",
+			"flying squirrels can be nocturnal",
         ];
     };
 
