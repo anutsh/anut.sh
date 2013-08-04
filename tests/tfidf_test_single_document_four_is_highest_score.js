c1fb00cf53119c9c5a5192127d71a4f49f7ecd86
zdwolfe <wolfe.zach@gmail.com>
2013-08-03 20:34:57 -0700

    syntax error, whoops

--- tests/tfidf_test_single_document_four_is_highest_score.js
+++ tests/tfidf_test_single_document_four_is_highest_score.js
@@ -1,5 +1,27 @@
 var tfidf = require('../tfidf');
 var redis = require('redis').createClient();
+
+function runTest() {
+    var totalTermCount = 0;
+    for (var prop in documents[documents.length - 1]) {
+        if (documents[documents.length - 1].hasOwnProperty(prop)) {
+            totalTermCount+=documents[documents.length-1][prop];
+        }
+    }
+    tfidf.getScores(documents[documents.length-1], totalTermCount, function(scores) {
+        var fail = false;
+        for (var param in scores) {
+            if (scores.hasOwnProperty(param)) {
+                if (param !== 'four' && 
+                    scores[param] >= scores.four) {
+                    fail = true;
+                }
+            }
+        }
+        console.log('Test has pased: ' + (fail === false));
+    });
+}
+
 var documents = [];
 documents.push({
     'one': 1,
@@ -22,23 +44,3 @@ for (var key in documents[documents.length - 1]) {
     }
 }
 
-function runTest() {
-    var totalTermCount = 0;
-    for (var prop in documents[documents.length - 1]) {
-        if (documents[documents.length - 1].hasOwnProperty(prop)) {
-            totalTermCount+=documents[documents.length-1][prop];
-        }
-    }
-    tfidf.getScores(documents[documents.length-1], totalTermCount, function(scores) {
-        var fail = false;
-        for (var param in scores) {
-            if (scores.hasOwnProperty(param)) {
-                if (param !== 'four' && 
-                    scores[param] >= scores.four) {
-                    fail = true;
-                }
-            }
-        }
-        console.log('Test has pased: ' + (fail === false));
-    });
-}
