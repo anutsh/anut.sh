8cfe8f7917d114d2173d3684021fc8bf7fcfe0c0
zdwolfe <wolfe.zach@gmail.com>
2013-08-03 19:37:04 -0700

    Refactored tfidf, hasn't been tested

--- tfidf.js
+++ tfidf.js
@@ -1,39 +1,19 @@
 var redis = require('redis').createClient();
+var tfidf = {};
 
-// documentTerms: {term:count, term2:count2, ... } 
-tfidf.getScores = function (documentTerms, totalTermCount, cb) {
-    updateBackground(documentTerms, function(backgroundTotal) {
-        score = {};
-
-        var keysServied = 0;
-
-        for (var keyIndex = 0; keyIndex < keys.legth; keyIndex++) {
-            var key = keys[keyIndex];
-            var value = parseInt(documentTerms[key]);
-            redis.get(key, function (err, res) {
-                score[key] = ((value+1)/(backgroundTotal+20000)) / totalTermCount / parseInt(res);
-                keysServied ++;
-                if (keysServiced === documentTerms.length) {
-                    return cb(score);
-                }
-            });
-        }
-    });
-}
-
-tfidf.updateBackground = function (documentTerms, cb) {
+function updateBackground (documentTerms, cb) {
     var totalVal = 0;
-    var keysServied = 0;
+    var keysServiced = 0;
     for (var key in documentTerms) {
-        var value = parseInt(documentTerms[key]);
+        var value = parseInt(documentTerms[key], 10);
         totalVal += value;
 
         redis.get(key, function (err, res) {
             var delta = 0;
-            if(res == null) {
+            if(res === null) {
                 delta = value;
             } else {
-                delta = parseInt(res) + value;
+                delta = parseInt(res, 10) + value;
             }
             redis.set(key, delta, function(err, res) {
                 if(err) {
@@ -43,12 +23,12 @@ tfidf.updateBackground = function (documentTerms, cb) {
                 if (keysServiced === documentTerms.length) {
                     redis.get("TOTAL_KEY", function (err, res) {
                         var totalKeyDelta = 0;
-                        if(res == null) {
+                        if(res === null) {
                             // key does not exist. set a new key-value pair.
                             totalKeyDelta = totalVal;
                         } else {
                             // key exists. update the value.
-                            totalKeyDelta = parseInt(res) + totalVal;
+                            totalKeyDelta = parseInt(res, 10) + totalVal;
                         }
                         redis.set("TOTAL_KEY", totalKeyDelta, function(err, res) {
                             if (err) {
@@ -61,7 +41,28 @@ tfidf.updateBackground = function (documentTerms, cb) {
             });
         });
     }
-
 }
 
-export = tfidf;
+// documentTerms: {term:count, term2:count2, ... } 
+tfidf.getScores = function (documentTerms, totalTermCount, cb) {
+    updateBackground(documentTerms, function(backgroundTotal) {
+        var score = {};
+
+        var keysServiced = 0;
+
+        var getCallback = function(err, res) {
+            score[key] = ((value+1)/(backgroundTotal+20000)) / totalTermCount / parseInt(res, 10);
+            keysServiced ++;
+            if (keysServiced === documentTerms.length) {
+                return cb(score);
+            }
+        };
+        for (var keyIndex = 0; keyIndex < documentTerms.legth; keyIndex++) {
+            var key = documentTerms[keyIndex];
+            var value = parseInt(documentTerms[key], 10);
+            redis.get(key, getCallback);
+        }
+    });
+};
+
+exports.getScores = tfidf.getScores;
