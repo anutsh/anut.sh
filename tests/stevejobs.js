583dc4ea0bf09f698bfd09a4eed9fb77d432ed35
zdwolfe <wolfe.zach@gmail.com>
2013-08-10 23:09:33 -0700

    Move tests. Remove stevejobs test

--- tests/stevejobs.js
+++
@@ -1,5 +0,0 @@
-var request = require('request');
-request('http://en.wikipedia.org/wiki/World_War_2', function(err, res, body) {
-    console.log('err = ' + err);
-    console.log('res = ' + (body));
-});
